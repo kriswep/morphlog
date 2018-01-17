0d3b7ee52a0423803abd99e9e548290aca5e012a
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-01-17 23:53:38 +0100

    allow adding members to projects

--- src/resolvers/Mutation/project.ts
+++ src/resolvers/Mutation/project.ts
@@ -17,4 +17,33 @@ export const project = {
     )
   },
 
+  async addProjectMember(parent, { user, project }, ctx: Context, info) {
+    const userId = getUserId(ctx)
+    const isProjectAdmin = await ctx.db.exists.Project({
+      id: project,
+      admin_some: { id: userId },
+    })
+    const userExists = await ctx.db.exists.User({
+      id: user,
+    })
+    if (!isProjectAdmin) {
+      throw new Error(`Project not found or you're not an admin`)
+    }
+    if (!userExists) {
+      throw new Error(`User does not exist`)
+    }
+
+    return ctx.db.mutation.updateProject(
+      {
+        where: { id: project },
+        data: {
+          member: {
+            connect: { id: user },
+          },
+        },
+      },
+      info,
+    )
+  },
+
 }
