03f96b51c838e4df52a570bb41385887f5a99949
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-01-21 21:54:52 +0100

    setup projects page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from './component/Home';
 import Profile from './component/Profile';
+import Projects from './component/Projects';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/profile" component={Profile} />
+          <Route exact path="/projects" component={Projects} />
         </Switch>
       </div>
     );
---
+++ client/src/component/Projects.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Projects = props => {
+  return <h2>Projects TODO</h2>;
+};
+
+export default Projects;
