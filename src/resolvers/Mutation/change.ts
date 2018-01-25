3001fd5e93be66c8aaced2af22331eaa8b753536
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-01-25 22:46:40 +0100

    update change mutation

--- src/resolvers/Mutation/change.ts
+++ src/resolvers/Mutation/change.ts
@@ -20,4 +20,38 @@ export const change = {
       info,
     );
   },
+
+  async updateChange(
+    parent,
+    { changeId, projectId, text },
+    ctx: Context,
+    info,
+  ) {
+    const userId = getUserId(ctx);
+
+    const isAuthor = ctx.db.exists.Change({
+      id: changeId,
+      author: { id: userId },
+    });
+
+    const isAdmin = ctx.db.exists.Project({
+      id: projectId,
+      admin_some: { id: userId },
+    });
+    if (!await isAuthor && !await isAdmin) {
+      throw new Error(`Not your Change`);
+    }
+
+    await isUserProjectAllowed(ctx, projectId);
+
+    return ctx.db.mutation.updateChange(
+      {
+        where: { id: changeId },
+        data: {
+          text,
+        },
+      },
+      info,
+    );
+  },
 };
