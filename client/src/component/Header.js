9425c4e3dba00e9404ce264148a92b814410599c
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-01-28 21:46:15 +0100

    improve header

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -1,37 +1,29 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Icon } from 'semantic-ui-react';
 
 import media from '../styles/media';
 
-const HeaderContainer = styled.div`
-  clip-path: polygon(0 0, 100% 0, 100% calc(100% - 1.45rem), 0 100%);
+const HeaderContainer = styled.header`
   grid-area: header;
-  background: ${props => props.theme.main};
-  border-bottom: solid 1px ${props => props.theme.lightestAccent};
-  padding: 1rem 0.25rem;
-  display: grid;
-  grid-gap: 3px;
-  grid-template-areas: 'nav' 'title';
-  ${media.m`
-    grid-gap: 10px;
-    padding 1rem 0.75rem;
-  `};
 `;
 
 const activeItem = 'home';
 const Header = props => (
   <HeaderContainer>
-    <Menu pointing secondary>
+    <Menu color="teal" size="huge" pointing secondary>
       <Menu.Item as={Link} to="/" active={props.match.isExact}>
+        <Icon size="large" name="home" />
         Home
       </Menu.Item>
       <Menu.Item as={NavLink} to="/project">
+        <Icon size="large" name="space shuttle" />
         Projects
       </Menu.Item>
       <Menu.Menu position="right">
         <Menu.Item as={NavLink} to="/profile">
+          <Icon size="large" name="user circle outline" />
           Profile
         </Menu.Item>
       </Menu.Menu>
