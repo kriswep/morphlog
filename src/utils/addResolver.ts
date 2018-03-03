8ba86525fe11571c7064c627b6dc44f4cae39ed7
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-03-03 21:58:54 +0100

    add semantic ts check to jest

--- src/utils/addResolver.ts
+++ src/utils/addResolver.ts
@@ -11,3 +11,29 @@ const addResolver = resolver => {
 };
 
 export default addResolver;
+
+// export interface Resolver {
+//   (parent: Object, args: Object, context: Object, info: Object): Promise<
+//     Resolver
+//   >;
+//   addResolver?: Function;
+// }
+
+// const addResolver = (resolver: Resolver): Resolver => {
+//   const baseResolver = resolver;
+//   baseResolver.addResolver = childResolver => {
+//     const newResolver = async (
+//       parent: Object,
+//       args: Object,
+//       context: Object,
+//       info: Object,
+//     ) => {
+//       await resolver(parent, args, context, info);
+//       return childResolver(parent, args, context, info);
+//     };
+//     return addResolver(newResolver);
+//   };
+//   return baseResolver;
+// };
+
+// export default addResolver;
