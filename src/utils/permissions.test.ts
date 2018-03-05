6dc64f33d6939e69103b27d64fba6cd632e2cd03
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-03-05 22:55:23 +0100

    add projectAccess permission test

--- src/utils/permissions.test.ts
+++ src/utils/permissions.test.ts
@@ -1,8 +1,8 @@
 /* globals test expect */
-import { requiresAuth } from './permissions';
+import { requiresAuth, requiresProjectAccess } from './permissions';
 import * as jwt from 'jsonwebtoken';
 
-test('requiresAuth should not throw if authenticated with token', async () => {
+test('requiresAuth should not throw if authenticated with token', () => {
   const auth = jwt.sign({ userId: 1 }, process.env.APP_SECRET);
 
   const userId = requiresAuth('', '', {
@@ -11,7 +11,7 @@ test('requiresAuth should not throw if authenticated with token', async () => {
   expect(userId).toBe(1);
 });
 
-test('requiresAuth should throw if trying to authenticate with false token', async () => {
+test('requiresAuth should throw if trying to authenticate with false token', () => {
   const invalidTokenAuth = jwt.sign({ userId: 1 }, 'malicious secret');
 
   expect(
@@ -24,10 +24,62 @@ test('requiresAuth should throw if trying to authenticate with false token', asy
   ).toThrow('Not Authenticated');
 });
 
-test('requiresAuth should throw if not authenticated', async () => {
+test('requiresAuth should throw if not authenticated', () => {
   const user = {};
 
   expect(
     requiresAuth.bind(null, '', '', { request: { get: () => {} } }),
   ).toThrow('Not Authenticated');
+});
+
+test('requiresProjectAccess should allow if authenticated and project member', async () => {
+  const auth = jwt.sign({ userId: 1 }, process.env.APP_SECRET);
+
+  const access = await requiresProjectAccess(
+    '',
+    { projectId: 2 },
+    {
+      request: { get: () => auth },
+      db: { exists: { Project: () => true } },
+    },
+  );
+  expect(access).toBeTruthy();
+});
+
+test('requiresProjectAccess should throw if authenticated and wrong project member', async () => {
+  const auth = jwt.sign({ userId: 1 }, process.env.APP_SECRET);
+
+  let message;
+  try {
+    await requiresProjectAccess(
+      '',
+      { id: 2 },
+      {
+        request: { get: () => auth },
+        db: { exists: { Project: async () => false } },
+      },
+    );
+  } catch (error) {
+    message = error.message;
+  }
+  expect(message).toBe('No Access');
+});
+
+test('requiresProjectAccess should throw if authenticated without projectID', async () => {
+  const auth = jwt.sign({ userId: 1 }, process.env.APP_SECRET);
+
+  let message;
+  try {
+    await requiresProjectAccess(
+      '',
+      {}, // no projectId nor id
+      {
+        request: { get: () => auth },
+        db: { exists: { Project: async () => true } },
+      },
+    );
+  } catch (error) {
+    message = error.message;
+  }
+  expect(message).toBe('No Access');
 });
