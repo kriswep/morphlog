35ad73005ddcb36ede06a72bbf2aaa87fef46503
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-03-13 22:12:45 +0100

    add AuthPayload test

---
+++ src/resolvers/AuthPayload.test.ts
@@ -0,0 +1,22 @@
+/* globals test expect jest */
+import { AuthPayload } from './AuthPayload';
+
+test('AuthPayload should have user member', async () => {
+  expect(AuthPayload.user).toBeDefined();
+});
+
+test('AuthPayload user should query fpr user', async () => {
+  const context = {
+    db: {
+      query: {
+        user: jest.fn(),
+      },
+    },
+  };
+
+  const user = AuthPayload.user({ user: { id: 1 } }, {}, context, { info: 2 });
+  expect(context.db.query.user).toHaveBeenCalledWith(
+    { where: { id: 1 } },
+    { info: 2 },
+  );
+});
