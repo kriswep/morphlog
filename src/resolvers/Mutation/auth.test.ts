9364ee7569a341b67f954bec3ef7fd81c733ea68
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-03-16 22:36:37 +0100

    test login

--- src/resolvers/Mutation/auth.test.ts
+++ src/resolvers/Mutation/auth.test.ts
@@ -1,4 +1,5 @@
 /* globals test expect jest */
+import * as bcrypt from 'bcryptjs';
 import * as jwt from 'jsonwebtoken';
 
 import { auth } from './auth';
@@ -29,4 +30,61 @@ test('auth should be able to signup and return token', async () => {
   };
   expect(userId).toEqual(1);
   expect(received.user).toEqual({ id: 1 });
+});
+
+test('auth should be able to login and return token', async () => {
+  const password = 'secret';
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = {
+    id: 1,
+    password: hashedPassword,
+  };
+  const context = {
+    db: {
+      query: {
+        user: jest.fn(() => user).mockImplementationOnce(() => false),
+      },
+    },
+  };
+
+  // first call should fail
+  let err = false;
+  try {
+    await auth.login({}, { email: 'invalid', password: 'secret' }, context, {
+      info: 2,
+    });
+  } catch (e) {
+    err = e;
+  }
+  expect(err.toString()).toEqual(
+    'Error: No such user found for email: invalid',
+  );
+
+  // invalid password
+  err = false;
+  try {
+    await auth.login({}, { email: 'mymail', password: 'wrong' }, context, {
+      info: 2,
+    });
+  } catch (e) {
+    err = e;
+  }
+  expect(err.toString()).toEqual('Error: Invalid password');
+
+  // good call
+  console.log(password);
+  const received = await auth.login(
+    {},
+    { email: 'mymail', password },
+    context,
+    {
+      info: 2,
+    },
+  );
+
+  const { userId } = jwt.verify(received.token, process.env.APP_SECRET) as {
+    userId: string;
+  };
+  expect(userId).toEqual(1);
+  expect(received.user).toEqual(user);
 });
