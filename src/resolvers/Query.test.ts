3652719ed03ffaed4ef3159cca17d4cc639df54c
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-03-21 22:04:47 +0100

    test project query

--- src/resolvers/Query.test.ts
+++ src/resolvers/Query.test.ts
@@ -1,4 +1,12 @@
+// import { requiresAuth, requiresProjectAccess } from '../utils/permissions';
+import { getUserId, isUserProjectAllowed } from '../utils';
+
 import { Query } from './Query';
+
+jest.mock('../utils', () => ({
+  getUserId: jest.fn(),
+  isUserProjectAllowed: jest.fn(),
+}));
 
 test('Query should have needed resolvers', async () => {
   expect(Query.project).toBeDefined();
@@ -6,4 +14,28 @@ test('Query should have needed resolvers', async () => {
   expect(Query.change).toBeDefined();
   expect(Query.changes).toBeDefined();
   expect(Query.me).toBeDefined();
+});
+
+test('query for project', async () => {
+  const context = {
+    db: {
+      query: {
+        project: jest.fn(() => true),
+      },
+    },
+  };
+
+  const received = await Query.project({}, { id: 1 }, context, {
+    info: 2,
+  });
+
+  // guards called?
+  expect(getUserId).toHaveBeenCalledTimes(1);
+  expect(isUserProjectAllowed).toHaveBeenCalledTimes(1);
+  // check resolver
+  expect(received).toBeTruthy();
+  expect(context.db.query.project).toBeCalledWith(
+    { where: { id: 1 } },
+    { info: 2 },
+  );
 });
