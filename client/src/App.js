560e1dd4a1c244c9cd34bffc5b601391a3c069b7
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-03-23 22:17:13 +0100

    cleanup app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import styled, { ThemeProvider } from 'styled-components';
 
 import Header from './component/Header';
 import Home from './component/Home';
@@ -8,50 +7,15 @@ import Profile from './component/Profile';
 import Projects from './component/Projects';
 import Project from './component/Project';
 
-import theme from './styles/theme';
-import media from './styles/media';
-
-const Main = styled.section`
-  min-height: 100vh;
-  color: ${props => props.theme.darkShades};
-  background: ${props => props.theme.lightShades};
-  line-height: 1.625rem;
-  display: grid;
-  grid-gap: 3px;
-  grid-template-columns: 17.5rem 1fr;
-  grid-template-rows: auto 1fr auto auto;
-  grid-template-areas:
-    'header header' 'content content' 'sidebar sidebar' 'footer footer';
-  ${media.m`
-  grid-gap: 10px;
-  grid-template-rows: auto 1fr auto;
-  grid-template-areas: 'header header header' 'sidebar content content' 'footer footer footer';
-  `} ${media.l`
-    grid-template-columns: 20rem 1fr;
-  `} ${media.xl`
-    grid-template-columns: 35rem 1fr;
-  `} ${media.xxl`
-    grid-template-columns: 50rem 1fr;
-  `} ${media.xxxl`
-    grid-template-columns: 70rem 1fr;
-  `} ${media.uxxxl`
-    grid-template-columns: 100rem 1fr;
-  `};
-`;
-
 class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        {/* <Main className="App"> */}
-        <div>
-          <Route path="/" component={Header} />
-          <Route exact path="/" component={Home} />
-          <Route path="/project/:projectId?" component={Projects} />
-          <Route exact path="/profile" component={Profile} />
-        </div>
-        {/* </Main> */}
-      </ThemeProvider>
+      <div>
+        <Route path="/" component={Header} />
+        <Route exact path="/" component={Home} />
+        <Route path="/project/:projectId?" component={Projects} />
+        <Route exact path="/profile" component={Profile} />
+      </div>
     );
   }
 }
