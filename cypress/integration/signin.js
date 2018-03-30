b6e65f1df93e31a7de278d317500fd2ed6e6a866
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-03-30 14:28:40 +0200

    add signIn integration test

---
+++ cypress/integration/signin.js
@@ -0,0 +1,57 @@
+describe('SignIn', function() {
+  it('opens signIn page', function() {
+    cy.visit('http://localhost:3030');
+
+    cy.get('a[href="/profile"]').click();
+
+    // Should be on a new URL which includes '/profile'
+    cy.url().should('include', '/profile');
+
+    // Should render authenticate
+    cy.get('[data-test="authenticate"]');
+  });
+
+  it('dont signIn with wrong email', function() {
+    // fill out form
+    cy.get('input[name="email"]').type('wrong@example.com');
+    cy.get('input[name="password"]').type('nooneknows');
+
+    // submit signIn action
+    cy.contains('button', 'SignIn').click();
+
+    // should show error message
+    cy
+      .get('[data-test="authenticate"]')
+      .contains('No such user found for email: wrong@example.com');
+  });
+
+  it('dont signIn with wrong password', function() {
+    // fill out form
+    cy.get('input[name="email"]').type('developer@example.com');
+    cy.get('input[name="password"]').type('wrong');
+
+    // submit signIn action
+    cy.contains('button', 'SignIn').click();
+
+    // should show error message
+    cy.get('[data-test="authenticate"]').contains('Invalid password');
+  });
+
+  it('signIn with right credentials', function() {
+    // fill out form
+    cy.get('input[name="email"]').type('developer@example.com');
+    cy.get('input[name="password"]').type('nooneknows');
+
+    // submit signIn action
+    cy.contains('button', 'SignIn').click();
+
+    // should show profile
+    cy.get('[data-test="profile"]').contains('developer@example.com');
+
+    // should logout
+    cy.contains('button', 'LogOut').click();
+
+    // Should show authenticate
+    cy.get('[data-test="authenticate"]');
+  });
+});
--- cypress/integration/simple_spec.js
+++
@@ -1,5 +0,0 @@
-describe('My First Test', function() {
-  it('Visits Morphlog', function() {
-    cy.visit('http://localhost:3030');
-  });
-});
