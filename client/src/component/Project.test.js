b2675b138ca36df3254e0c98743075e15410b423
Christoph Benjamin Weber <kriswep@wetainment.com>
2018-09-06 00:01:03 +0200

    cleanup project test

--- client/src/component/Project.test.js
+++ client/src/component/Project.test.js
@@ -2,13 +2,10 @@
 // TODO checkout https://github.com/react-cosmos/react-cosmos#react-apollo-graphql
 
 import React from 'react';
-// import { ApolloProvider } from 'react-apollo';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
-// import createClient from '../../utils/apolloMocks';
-// import Changes from './Changes';
-import { getData, PreMockedProvider } from '../../utils/apolloMocks2';
 
+import { getData, PreMockedProvider } from '../../utils/apolloMocks2';
 import Project, { PROJECT_QUERY } from './Project';
 
 jest.mock('./Changes', () => () => <div>MockedChange</div>);
@@ -25,7 +22,6 @@ const mocks = {
 
   // Mutation: () => ...
 };
-// const client = createClient(mocks);
 
 test('Projects renders correctly', async () => {
   const args = { id: 'cjcp94wxp025801100npb28yg' };
@@ -41,11 +37,6 @@ test('Projects renders correctly', async () => {
     </PreMockedProvider>,
   );
 
-  // const wrapper = mount(
-  //   <ApolloProvider client={client}>
-  //     <Project projectId="cjcp94wxp025801100npb28yg" />
-  //   </ApolloProvider>,
-  // );
   await new Promise(res => window.setTimeout(res, 1));
   wrapper.setProps({ projectId: 'bar' }); // poke it to rerender...
   expect(toJson(wrapper.find('[data-test="project"]'))).toMatchSnapshot();
